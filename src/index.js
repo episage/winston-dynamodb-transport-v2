213551476cbf36b9732ad79813cb819b959e7dbe
Tomasz Ciborski <tilosag@gmail.com>
2021-05-27 13:26:42 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -1,80 +1,83 @@
 var _ = require('lodash');
 var joi = require('joi');
 var AWS = require('aws-sdk');
-var TransportStream = require('winston-transport');
+var Transport = require('winston-transport');
 var uuid = require('uuid').v4;
 var withStack = require('./with-stack');
 
-module.exports = class DynamoDb extends TransportStream {
+module.exports = function (__options) {
+    Transport.call(this, options);
+    super(__options);
 
-    constructor(__options) {
-        super(__options);
+    this.name = __options.name || 'DynamoDB';
 
-        this.name = __options.name || 'DynamoDB';
+    // defaults
+    var _options = _.defaults({}, _options, {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        region: process.env.AWS_REGION,
+    })
 
-        // defaults
-        var _options = _.defaults({}, _options, {
-            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-            region: process.env.AWS_REGION,
-        })
-        // validation
-        var { error, value: awsOptions } = joi.object({
-            accessKeyId: joi.string().min(1),
-            secretAccessKey: joi.string().min(1),
-            region: joi.string().min(1),
-        }).validate(_options, { stripUnknown: true, presence: 'required' });
-        if (error) {
-            throw error;
-        }
-        this.awsOptions = awsOptions;
-        // validation
-        var { error, value: loggerOptions } = joi.object({
-            tableName: joi.string().min(1),
-        }).validate(_options, { stripUnknown: true, presence: 'required' });
-        if (error) {
-            throw error;
-        }
-        this.loggerOptions = loggerOptions;
-        // init
-        var dynamoDb = new AWS.DynamoDB(awsOptions)
-        this.documentClient = new AWS.DynamoDB.DocumentClient({
-            service: dynamoDb,
-        });
+    // validation
+    var { error, value: awsOptions } = joi.object({
+        accessKeyId: joi.string().min(1),
+        secretAccessKey: joi.string().min(1),
+        region: joi.string().min(1),
+    }).validate(_options, { stripUnknown: true, presence: 'required' });
+    if (error) {
+        throw error;
     }
+    this.awsOptions = awsOptions;
 
-    log(info, next) {
-        if (!next) {
-            next = noop;
-        }
+    // validation
+    var { error, value: loggerOptions } = joi.object({
+        tableName: joi.string().min(1),
+    }).validate(_options, { stripUnknown: true, presence: 'required' });
+    if (error) {
+        throw error;
+    }
+    this.loggerOptions = loggerOptions;
+    
+    // init
+    var dynamoDb = new AWS.DynamoDB(awsOptions)
+    this.documentClient = new AWS.DynamoDB.DocumentClient({
+        service: dynamoDb,
+    });
 
-        var mustHave = {
-            id: uuid(),
-            level: info.level,
-            timestamp: Date.now(),
-            message: info.message,
-        };
-        var optional = info.metadata || {};
-
-        // check for metadata - base key collisions
-        Object.keys(mustHave).forEach(k => {
-            if (k in optional) {
-                throw Error(`you must use a different metadata key for [${k}] because [${k}] is reserved`);
+    return {
+        log(info, next) {
+            if (!next) {
+                next = noop;
             }
-        })
 
-        var item = _.defaults({}, mustHave, info.metadata);
-        withStack(this.documentClient.put({
-            TableName: this.loggerOptions.tableName,
-            ReturnValues: 'NONE',
-            Item: item,
-        }).promise()).then(() => {
-            next(null, true);
-        }).catch(error => {
-            next(error);
-        });
+            var mustHave = {
+                id: uuid(),
+                level: info.level,
+                timestamp: Date.now(),
+                message: info.message,
+            };
+            var optional = info.metadata || {};
 
-        return true;
+            // check for metadata - base key collisions
+            Object.keys(mustHave).forEach(k => {
+                if (k in optional) {
+                    throw Error(`you must use a different metadata key for [${k}] because [${k}] is reserved`);
+                }
+            })
+
+            var item = _.defaults({}, mustHave, info.metadata);
+            withStack(this.documentClient.put({
+                TableName: this.loggerOptions.tableName,
+                ReturnValues: 'NONE',
+                Item: item,
+            }).promise()).then(() => {
+                next(null, true);
+            }).catch(error => {
+                next(error);
+            });
+
+            return true;
+        }
     }
 }
 
